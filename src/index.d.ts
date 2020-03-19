d2e9dd7957c3f32f0cc699c617d70a9c881bf73c
erodriguezh <taishogun@gmail.com>
2020-03-19 18:42:25 +0100

    git init

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { Common } from './sim-info.common';
+export declare class SimInfo extends Common {
+  // define your typings manually
+  // or..
+  // take the ios or android .d.ts files and copy/paste them here
+}
---
+++ src/sim-info.ios.ts
@@ -0,0 +1,5 @@
+import { Common } from './sim-info.common';
+
+export class SimInfo extends Common {
+
+}
