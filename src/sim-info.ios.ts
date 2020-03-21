3f23045656e5bfa09c887d5050dc6031ca3320fd
erodriguezh <taishogun@gmail.com>
2020-03-21 13:23:02 +0100

    add ios dev

--- src/sim-info.ios.ts
+++ src/sim-info.ios.ts
@@ -1,5 +1,25 @@
-import { Common } from './sim-info.common';
+import { SimData } from './sim-info.common';
 
-export class SimInfo extends Common {
+export class SimInfo {
+  getData(): Promise<SimData | SimData[]> {
+    const networkInfo = new CTTelephonyNetworkInfo();
+    const providers = networkInfo.serviceSubscriberCellularProviders;
 
+    if (Object.keys(providers).length > 0) {
+      const providersData = [];
+      for (let carrierIndex in providers) {
+        const carrier: CTCarrier = providers[carrierIndex];
+        providersData.push({
+          allowsVOIP: carrier.allowsVOIP,
+          carrierName: carrier.carrierName,
+          isoCountryCode: carrier.isoCountryCode,
+          mcc: carrier.mobileCountryCode,
+          mnc: carrier.mobileNetworkCode,
+        });
+      }
+      return Promise.resolve(providersData.length === 1 ? providersData[0] : providersData);
+    } else {
+      return Promise.reject('Sim information is unaccessible');
+    }
+  }
 }
